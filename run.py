e1bdb57e0cf939ba0c675af6e298e68e562c809a
Natalie Lockyer <Natalie_lockyer@outlook.com>
2023-11-05 12:10:52 +0000

    Added imports (random and word list)

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#Random words
+import random
+
+#Access to the list of words 
+from words import secret_word_list
+
