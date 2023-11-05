875b1db19f9fbae205736b072c373efa888d16e1
Natalie Lockyer <Natalie_lockyer@outlook.com>
2023-11-05 19:03:10 +0000

    Guess the letter function added. I have included a while loop etc - still working on this

---
+++ hangman_tries.py
@@ -0,0 +1,73 @@
+def get_hangman_stage(tries):
+    """
+    This function creates a list of string for each element of the hangman. For a string
+    to draw the handman I have used a multiline strings using triple quotes.     
+    The user will have a maximum of 6 lives. 
+    **READme Reference - for the stage design I have used codefather.tech.blog.com**
+    """
+    maximum_tries = 6
+    stages = ["""
+        ------
+        |    |
+        |
+        |
+        |
+        |
+        |
+    ------------
+    """, """
+        ------
+        |    |
+        |    O
+        |
+        |
+        |
+        |
+    ------------
+    """, """
+        ------
+        |    |
+        |    O
+        |    |
+        |    |
+        |
+        |
+    ------------
+    """, """
+        ------
+        |    |
+        |    O
+        |    |
+        |    |
+        |   /
+        |
+    ------------
+    """, """
+        ------
+        |    |
+        |    O
+        |    |
+        |    |
+        |   / \\
+        |
+    ------------
+    """, """
+        ------
+        |    |
+        |    O
+        |  --|
+        |    |
+        |   / \\
+        |
+    ------------
+    """, """
+        ------
+        |    |
+        |    O
+        |  --|--
+        |    |
+        |   / \\
+        |
+    ------------
+    """]
+    return stages[maximum_tries - tries]
--- run.py
+++ run.py
@@ -17,7 +17,7 @@ print("-------------------------------------------------------------------------
 #Enter your name
 username = input("Please enter your name: ")
 print("\n")
-print(f"Welcome to Hangman {username}, lets get started!")
+print(f"Welcome to Hangman {username}!\n")
 
 def get_word(secret_word_list):
     """
@@ -25,10 +25,42 @@ def get_word(secret_word_list):
     """
     return random.choice(secret_word_list)
 
+def display_secret_word(secret_word):
+    """
+    This function is to display the secret word, and replace the letters with underscores"""
+    print(" _ " * len(secret_word))
+
+def guess_the_letter(guessed_letter, secret_word):
+    """
+    This function asked the user to guess a letter,
+    if user has already used that letter - print - already been used
+    wrong letter - print - incorrect letter
+    correct letter - print - correct letter 
+    """
+    guessed_letter = ""
+    attempts_left = 6
+
+    while not guessed_letter and attempts_left == 0:
+        guess = input("Please enter a letter: ").lower()
+        if len(guess) > 1 and guessed_letter.isalphha():
+            print("Only single letters are allowed, please try again")
+        elif guess in guessed_letter:
+            print("You have already tried the letter", guess)
+        elif guess not in secret_word:
+            print(guess, "is not in the word")
+            attempts_left -= 1
+            guessed_letter.append(guess)
+        else:
+            print(f"Well done {username}, you guessed the correct letter")
+            guessed_letter.append(guess)
+        
 
 
-# def play_game(get_word)
-# secret_word = " _ " * len()
+print(f"To begin, please choose your first letter. \n")
 
-print(get_word(secret_word_list))
+secret_word = get_word(secret_word_list)
+display_secret_word(secret_word) 
+guess_the_letter(guess_the_letter, secret_word)
+
+print(hangman_tries.get_hangman_stage(attempts_left))
 
