5577629507cfa3e2cd7aadafe45e22c3c8de600b
Natalie Lockyer <Natalie_lockyer@outlook.com>
2023-11-14 17:47:43 +0000

    JSON Error - updated ready for Heroku

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file('leaderboard.json')
+CREDS = Credentials.from_service_account_file('cred.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hangman_leaderboard')
